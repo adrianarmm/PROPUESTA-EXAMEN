e4624cd53cd89bfc15b1843e709b137393ea8526
adrianarmm <areyemor@myuax.com>
2023-11-07 12:01:07 +0100

    20

--- PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
+++ PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
@@ -5,7 +5,7 @@ int main() {
     for (int a = -3; a <= 3; a++) {
         for (int b = -4; b <= 4; b++) {
             int c = sqrt(a * a + b * b);
-            if (a * a + b * b == c * c) {
+            if (c*c == a * a + b * b  && c <= 5) {
                 std::cout << a << " - " << b << " - " << c << std::endl;
             }
 
