7e9e0d4c98fd420bbe32f5c683c0343d3ea4d43d
adrianarmm <areyemor@myuax.com>
2023-11-07 11:30:31 +0100

    8

--- PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
+++ PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
@@ -18,10 +18,44 @@ int main() {
             if (f * f == d * d + e * e && f <= 10) {
                 std::cout << d << " - " << e << " - " << f << std::endl;
             }
-            
+
         }
         
     }
+    int main()
+    for (int g = -5; g <= 5; g++) {
+        for (int h = -12; h <= 12; h++) {
+            int i = sqrt(g * g + h * h);
+            if (i * i == g * g + h * h && i <= 13) {
+                std::cout << g << " - " << h << " - " << i << std::endl;
+            }
 
-    
+        }
+
+    }
+    int main() {
+        for (int j = -7; j <= 7; j++) {
+            for (int k = -24; k <= 24; k++) {
+                int l = sqrt(j * j + k * k);
+                if (l * l == j * j + k * k && l <= 25) {
+                    std::cout << j << " - " << k << " - " << l << std::endl;
+                }
+
+            }
+
+        }
+        int main() {
+            for (int m = -8; m <= 8; m++) {
+                for (int n = -15; n <= 15; n++) {
+                    int o = sqrt(m * m + n * n);
+                    if (o * o == m * m + n * n && o <= 17) {
+                        std::cout << m << " - " << n << " - " << o << std::endl;
+                    }
+
+                }
+
+            }
+
+
+        }
 }
