e582dfc7e906199fb99d51906991d2bf354d3d7a
adrianarmm <areyemor@myuax.com>
2023-11-07 11:31:38 +0100

    9

--- PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
+++ PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
@@ -11,7 +11,7 @@ int main() {
 
         }
     }
-int main() {
+int main();
     for (int d = -6; d <= 6; d++) {
         for (int e = -8; e <= 8 ; e++) {
             int f = sqrt(d * d + e * e);
@@ -22,7 +22,7 @@ int main() {
         }
         
     }
-    int main()
+    int main();
     for (int g = -5; g <= 5; g++) {
         for (int h = -12; h <= 12; h++) {
             int i = sqrt(g * g + h * h);
@@ -33,7 +33,7 @@ int main() {
         }
 
     }
-    int main() {
+    int main();
         for (int j = -7; j <= 7; j++) {
             for (int k = -24; k <= 24; k++) {
                 int l = sqrt(j * j + k * k);
@@ -44,7 +44,7 @@ int main() {
             }
 
         }
-        int main() {
+        int main();
             for (int m = -8; m <= 8; m++) {
                 for (int n = -15; n <= 15; n++) {
                     int o = sqrt(m * m + n * n);
@@ -55,7 +55,6 @@ int main() {
                 }
 
             }
-
-
+    return 0;
         }
 }
