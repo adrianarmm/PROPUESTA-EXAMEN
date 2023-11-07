0f6af252ace8f2c83ce457d481bcd8192c55c42c
adrianarmm <areyemor@myuax.com>
2023-11-07 11:11:48 +0100

    3

--- PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
+++ PROPUESTA EXAMEN/PROPUESTA EXAMEN.cpp
@@ -9,6 +9,14 @@ int main ()
     if (c * c == i * i + j * j && c)
         std::cout << i << " - " << j << " - " << c << std::endl;
 
-
     return 0;
+    {
+        int a = 6
+        int b = 8
+        int c = 10
+        if (c * c == a * a + b * b && c)
+            std::cout << a << " - " << b << " - " << c << std::endl;
+    }
+    return 0;
+
 }
